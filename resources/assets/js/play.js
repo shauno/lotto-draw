2026df096a26d6db9c62bab4da71f1bd70bee6bb
Shaun Alberts <shaunalberts@gmail.com>
2016-01-31 12:42:15 +0200

    Refactor main game JS into reusable function that will be used when power ball is implemented. JS code is getting a little messy now, might be time to reconsider jQuery and move to  dedicated front end library like Angular?

--- resources/assets/js/play.js
+++ resources/assets/js/play.js
@@ -3,50 +3,57 @@
     like Angular or React would be beneficial if this becomes a larger SPA with further features and interactions
 */
 
+function play(game) {
+    jQuery.ajax({
+        url: '/api/play-' + game,
+        dataType: 'json',
+        success: function(data) {
+            //Get the winning numbers into simple array
+            var numbers = data.numbers.map(function(num) {
+                return num.number;
+            });
+
+            /*
+             Output the numbers into the list
+
+             There will be an issue if you have set the winning ball count high enough that the list runs out
+             of space. We would need to consider how best to design the UI to handle min/max/mean results if we
+             planned on have games with arbitrary large sets
+             */
+
+            var row = '' +
+                '<li class="list-group-item">' +
+                '<span class="badge">' + data.created_at + '</span>' + //obviously will be UTC as thats what the server is set to. If we want to show date in the user's local, we can fiddle with the JS Date() object
+                numbers.join('&nbsp;&nbsp;&nbsp;&nbsp;') +
+                '</li>';
+
+            jQuery('#' + game + '-list').prepend(row);
+
+            //only show 10 results, so remove superfluous items (:gt is zero indexed)
+            jQuery('#' + game + '-list li:gt(9)').remove();
+        },
+        error: function() {
+            jQuery('#' + game + '-error')
+                .text('There was a problem getting the numbers')
+                .fadeIn()
+                .removeClass('hidden');
+
+            setTimeout(function() {
+                jQuery('#' + game + '-error').fadeOut();
+            }, 5000);
+        }
+    });
+
+}
+
+
+
 jQuery(document).ready(function() {
 
     jQuery('#play-button').click(function(e) {
         e.preventDefault();
 
-        jQuery.ajax({
-            url: '/api/play-main',
-            dataType: 'json',
-            success: function(data) {
-                //Get the winning numbers into simple array
-                var numbers = data.numbers.map(function(num) {
-                    return num.number;
-                });
-
-                /*
-                    Output the numbers into the list
-
-                    There will be an issue if you have set the winning ball count high enough that the list runs out
-                    of space. We would need to consider how best to design the UI to handle min/max/mean results if we
-                    planned on have games with arbitrary large sets
-                */
-
-                var row = '' +
-                    '<li class="list-group-item">' +
-                        '<span class="badge">' + data.created_at + '</span>' + //obviously will be UTC as thats what the server is set to. If we want to show date in the user's local, we can fiddle with the JS Date() object
-                        numbers.join('&nbsp;&nbsp;&nbsp;&nbsp;') +
-                    '</li>';
-
-                jQuery('#main-list').prepend(row);
-
-                //only show 10 results, so remove superfluous items (:gt is zero indexed)
-                jQuery('#main-list li:gt(9)').remove();
-            },
-            error: function() {
-                jQuery('#main-error')
-                    .text('There was a problem getting the numbers')
-                    .fadeIn()
-                    .removeClass('hidden');
-
-                setTimeout(function() {
-                    jQuery('#main-error').fadeOut();
-                }, 5000);
-            }
-        });
+        play('main');
     });
 
 });
